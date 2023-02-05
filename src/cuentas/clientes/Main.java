e8e23dc8388ac76fcf38f2b9a8c7a7b41a0239e2
Jose <jagallegoc05@gmail.com>
2023-02-05 19:52:23 +0100

    Variable añadida

---
+++ src/cuentas/clientes/Main.java
@@ -0,0 +1,32 @@
+package cuentas.clientes;
+
+public class Main {
+boolean pruebaGit;
+    public static void main(String[] args) {
+        operativa_cuenta(null);
+        
+    }
+
+    private static void operativa_cuenta(Float Cantidad) {
+        Cuenta cuenta1;
+        double saldoActual;
+
+        cuenta1 = new Cuenta("Rafael Nadal Parera","1000-2365-85-1230456789",2500,0);
+        
+        System.out.println("El saldo actual es:"+ cuenta1.estado() );
+
+        try {
+            cuenta1.retirar(2300);
+            System.out.println("El saldo actual es:"+ cuenta1.estado() );
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(695);
+            System.out.println("El saldo actual es:"+ cuenta1.estado() );
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
